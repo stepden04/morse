0dd8e88306b6e7af614d6bfdeae332eb84b597e7
Your Name <you@example.com>
2025-06-07 12:32:08 +0100

    Added an encrypt function to turn txt into morse code (? means unknown character)

--- main.py
+++ main.py
@@ -3,16 +3,34 @@ from morse import *
 def decrypt(morse: str) -> str:
     message = ""
     letters = []
-    chunks = morse.split(" ")
+    chunks = [x for x in morse.split(" ") if x]
+    
     for t in chunks:
-        translated = MORSE_LETTERS[t]
+        translated = MORSE_LETTERS.get(t, "?")
         message += translated
         letters.append(translated)
     return " ".join(letters)
 
+def encrypt(text: str) -> str:
+    text = text.upper()
+    morse_code = []
+    for char in text:
+        if char == " ":
+            morse_code.append("")
+        elif char in LETTERS_MORSE:
+            morse_code.append(LETTERS_MORSE[char])
+        elif char == "":
+            pass
+        else:
+            morse_code.append("?")
+    return " ".join(morse_code)
+    
 def main():
-    print("Hello world!")
-    print(decrypt(".... . .-.. .-.. --- .-- --- .-. .-.. -.."))
-
+    msg = "Hello World"
+    morse_code = encrypt(msg)
+    text = decrypt(morse_code)
+    print(f"{msg} -> {morse_code}")
+    print(f"{morse_code} -> {text}")
+    
 if __name__ == "__main__":
     main()
